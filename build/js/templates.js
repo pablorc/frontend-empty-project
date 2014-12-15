ef1a2220e82103a17270042d57df38fdf1e9a4b5
Pablo Rivera Conde <pabloriveraconde@gmail.com>
2014-12-15 23:06:05 +0100

    Handlebars

---
+++ build/js/templates.js
@@ -0,0 +1,5 @@
+this["App"] = this["App"] || {};
+this["App"]["templates"] = this["App"]["templates"] || {};
+this["App"]["templates"]["index"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  return "<!DOCTYPE html>\n<html lang=\"en\">\n  <head>\n    <meta charset=\"utf-8\">\n    <title>title</title>\n    <link rel=\"stylesheet\" href=\"global.css\">\n    <script src=\"script.js\"></script>\n  </head>\n  <body>\n    <!-- page content -->\n  </body>\n</html>\n";
+  },"useData":true});
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var handlebars = require('gulp-handlebars');
+var wrap = require('gulp-wrap');
+var declare = require('gulp-declare');
+var concat = require('gulp-concat');
 
 var basefile = "./www";
 var sass_dir = basefile + "/sass";
-var css_dir = basefile + "/css";
+var markup_dir =  basefile + "/templates";
+
+var build_dir = "./build";
+var css_dir = build_dir + "/css";
+var js_dir = build_dir + "/js";
+
 
 
 gulp.task('default', function() {
@@ -15,4 +24,16 @@ gulp.task('sass', function () {
   gulp.src(sass_dir + "/**/*.{sass,scss}")
   .pipe(sass())
   .pipe(gulp.dest(css_dir));
+});
+
+gulp.task('markup', function(){
+  gulp.src(markup_dir + '/**/*.hbs')
+  .pipe(handlebars())
+  .pipe(wrap('Handlebars.template(<%= contents %>)'))
+  .pipe(declare({
+    namespace: 'App.templates',
+    noRedeclare: true, // Avoid duplicate declarations
+  }))
+  .pipe(concat('templates.js'))
+  .pipe(gulp.dest(js_dir));
 });
